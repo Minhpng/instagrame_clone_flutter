caea28ade44a9716aa6594f675beeb94a2bf49ae
minhpng <nguyenminh.236@gmail.com>
2023-05-11 12:52:49 +0700

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -11,56 +12,75 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+extension OptionalFixNullNumber<T extends num> on T? {
+  T? operator +(T? other) {
+    final shadow = this;
+    if (shadow != null) {
+      return shadow + (other ?? 0) as T;
+    } else {
+      return null;
+    }
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class Counter extends StateNotifier<int?> {
+  Counter() : super(null);
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  void increment() {
+    state = state == null ? 1 : state + 1;
+  }
+
+  void resetCounter() {
+    state = null;
+  }
+
+  // int? get value => state;
+}
+
+final counterProvider =
+    StateNotifierProvider<Counter, int?>((ref) => Counter());
+
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({super.key});
+
+  Consumer showCount() {
+    return Consumer(builder: (context, ref, child) {
+      final count = ref.watch(counterProvider);
+      final text = count ?? 'Press add button';
+      return Text(text.toString());
     });
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: showCount(),
+        actions: [
+          IconButton(
+              onPressed: ref.read(counterProvider.notifier).resetCounter,
+              icon: const Icon(Icons.restart_alt))
+        ],
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: Container(
+          child: showCount(),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: IconButton(
+        onPressed: ref.read(counterProvider.notifier).increment,
+        icon: Icon(Icons.add),
       ),
     );
   }
