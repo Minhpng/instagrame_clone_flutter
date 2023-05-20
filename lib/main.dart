476246adbcd91559e3663fefc001eabe4c03cc21
minhpng <nguyenminh.236@gmail.com>
2023-05-20 14:02:08 +0700

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:collection';
-
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:uuid/uuid.dart';
 
-void main() {
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
   runApp(const ProviderScope(child: MyApp()));
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -16,9 +18,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      darkTheme: ThemeData.dark(),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blueGrey,
+        indicatorColor: Colors.blueGrey,
+      ),
       themeMode: ThemeMode.dark,
       theme: ThemeData(
+        brightness: Brightness.dark,
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
@@ -27,96 +34,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-@immutable
-class Film {
-  final String id;
-  final String title;
-  final String description;
-  final bool isFavorite;
-  const Film({
-    required this.id,
-    required this.title,
-    required this.description,
-    required this.isFavorite,
-  });
-
-  Film copy({required bool isFavorite}) {
-    return Film(
-        id: id, title: title, description: description, isFavorite: isFavorite);
-  }
-
-  @override
-  String toString() {
-    return 'Film(id: $id, '
-        'title: $title, '
-        'description: $description, '
-        'isFavorite: $isFavorite)';
-  }
-
-  @override
-  bool operator ==(covariant Film other) =>
-      id == other.id && isFavorite == other.isFavorite;
-
-  @override
-  int get hashCode => Object.hashAll([id, isFavorite]);
-}
-
-const allFilms = [
-  Film(
-    id: '1',
-    title: 'The Shawshank Redemption',
-    description: 'Description for The Shawshank Redemption',
-    isFavorite: false,
-  ),
-  Film(
-    id: '2',
-    title: 'The Godfather',
-    description: 'Description for The Godfather',
-    isFavorite: true,
-  ),
-  Film(
-    id: '3',
-    title: 'The Godfather: Part II',
-    description: 'Description for The Godfather: Part II',
-    isFavorite: false,
-  ),
-  Film(
-    id: '4',
-    title: 'The Dark Knight',
-    description: 'Description for The Dark Knight',
-    isFavorite: false,
-  ),
-];
-
-class FilmsNotifier extends StateNotifier<List<Film>> {
-  FilmsNotifier() : super(allFilms);
-
-  void update(Film film) {
-    final newfilm = film.copy(isFavorite: !film.isFavorite);
-    state = state.map((thisFilm) {
-      if (film.id == thisFilm.id) {
-        return newfilm;
-      } else {
-        return thisFilm;
-      }
-    }).toList();
-  }
-}
-
-final allFilmsProvider =
-    StateNotifierProvider<FilmsNotifier, List<Film>>((_) => FilmsNotifier());
-
-enum FavoriteStatus { all, favorite, notFavorite }
-
-final favoriteStatusProvider =
-    StateProvider<FavoriteStatus>((_) => FavoriteStatus.all);
-
-final favoriteFilmsProvider = Provider<Iterable<Film>>(
-    (ref) => ref.watch(allFilmsProvider).where((film) => film.isFavorite));
-
-final notFavoriteFilmsProvider = Provider<Iterable<Film>>(
-    (ref) => ref.watch(allFilmsProvider).where((film) => !film.isFavorite));
-
 class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key});
 
@@ -124,27 +41,12 @@ class MyHomePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        centerTitle: true,
+        title: const Text(
+          'Home Page',
+        ),
       ),
-      body: Consumer(builder: (context, ref, child) {
-        final films = ref.watch(allFilmsProvider);
-        return ListView.builder(
-            itemCount: films.length,
-            itemBuilder: (context, index) {
-              print('rebuild: ${films[index].title}');
-              return ListTile(
-                  title: Text(films[index].title),
-                  subtitle: Text(films[index].description),
-                  trailing: IconButton(
-                    onPressed: () {
-                      ref.read(allFilmsProvider.notifier).update(films[index]);
-                    },
-                    icon: Icon(films[index].isFavorite
-                        ? Icons.favorite
-                        : Icons.favorite_border),
-                  ));
-            });
-      }),
+      body: Container(),
     );
   }
 }
